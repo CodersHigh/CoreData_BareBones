80e159f468837e770abd30e2e8b10e852e83a781
이로운 <lru040409@gmail.com>
2022-07-05 09:17:23 +0900

    Initial Commit

---
+++ CoreData_BareBone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreData_BareBone
+//
+//  Created by 이로운 on 2022/07/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoreData_BareBone/CoreData_BareBoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreData_BareBoneApp.swift
+//  CoreData_BareBone
+//
+//  Created by 이로운 on 2022/07/05.
+//
+
+import SwiftUI
+
+@main
+struct CoreData_BareBoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
